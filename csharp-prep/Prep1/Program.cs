bcf5737981fcfc62377453e6a721d6d45ba1588d
Celestia Wilkey <cwilkey4@byui.edu>
2023-01-05 17:32:24 -0700

    Assignment 1 has been completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
-using System;
+// Celestia Wilkey, 01/05/2022
+// This is our class' first assignment. We need to prompt the user for their name and then print it out in a "Bond, James Bond" style.
+using System; 
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) 
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string cwfirst = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string cwlast = Console.ReadLine();
+        Console.WriteLine("");
+        Console.Write($"Your name is {cwlast}, {cwfirst} {cwlast}.");
     }
 }
